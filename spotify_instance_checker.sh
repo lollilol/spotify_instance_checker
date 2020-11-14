c03801530c70b5d68c3456b95ca7bb8949b2449b
lollilol <38194372+lollilol@users.noreply.github.com>
2020-11-14 12:37:18 +0100

    add --uri option when opening a new instance

--- spotify_instance_checker.sh
+++ spotify_instance_checker.sh
@@ -4,5 +4,5 @@
 if ! wmctrl -ia $(wmctrl -lp | grep $(ps ax | grep '/opt/spotify/spotify' | grep -v grep | awk 'NR==1{print $1}') | awk 'NR==1{print $1} $3==pid {print $1; exit}') > /dev/null 2>&1
 then
 	# if not, start it
-	/opt/spotify/spotify &
+	/opt/spotify/spotify --uri=%U &
 fi
